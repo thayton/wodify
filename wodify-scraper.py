da0a67d8efad84fd3177288cbfc7852bbae51ee0
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-10-22 14:13:17 -0400

    switch to admin

--- wodify-scraper.py
+++ wodify-scraper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import re
 import time
 
 from bs4 import BeautifulSoup
@@ -28,7 +29,7 @@ class WodifyScraper(object):
 
         def logged_in(driver):
             try:
-                elem = driver.find_element_by_id('WebForm1')
+                elem = driver.find_element_by_link_text('WOD')
                 return elem.is_displayed()
             except NoSuchElementException:
                 return False
@@ -36,11 +37,37 @@ class WodifyScraper(object):
         wait = WebDriverWait(self.driver, 10)
         wait.until(logged_in)
 
+        #self.driver.save_screenshot('screenshot.png')
+
+    def switch_to_wodify_admin(self):
+        s = BeautifulSoup(self.driver.page_source, 'html.parser')
+        r = re.compile(r'^W_Theme_UI_wt\d+_block_wt\d_wtApplications$')
+        e = s.find(id=r)
+
+        elem = self.driver.find_element_by_id(e['id'])
+        text = 'Wodify Admin'
+
+        Select(elem).select_by_visible_text(text)
+
+        def admin_page_loaded(driver):
+            s = BeautifulSoup(driver.page_source, 'html.parser')
+            r = re.compile(r'^W_Theme_UI_wt\d+_block_wt\d_wtApplications$')
+            e = s.find(id=r)
+
+            elem = self.driver.find_element_by_id(e['id'])
+            text = 'Wodify Admin'
+
+            return Select(elem).first_selected_option.text == text
+
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(admin_page_loaded)
+
         self.driver.save_screenshot('screenshot.png')
-        
+
     def scrape(self):
         self.driver.get(self.url)
         self.login()
+        self.switch_to_wodify_admin()
 
 if __name__ == '__main__':
     scraper = WodifyScraper()
